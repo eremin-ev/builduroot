3e8982e6c6acbbdec68dba1bddc6b6207e2ea5b6
E.V.Eremin <e.vl.eremin@yandex.ru>
2020-10-17 00:51:58 +0300

    buildroot: add qemu/chroot README instructions

--- builduroot.sh
+++ builduroot.sh
@@ -390,5 +390,3 @@ cd $dir
 if [[ ${b_initrd} == "1" ]]; then
 	mk_initrd
 fi
-
-#qemu-system-x86_64 -drive file=hda.img,if=none,format=raw,id=vd0 -device virtio-blk-pci,drive=vd0 -smp cpus=2 -kernel boot/vmlinuz -initrd initrd.cpio -append "vga=792 init_on_free=1" -display sdl
